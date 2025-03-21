b2312ed7c9e1a8890201aa9547eddd8136a7545a
Rikard radovac <rikard.radovac@gmail.com>
2025-03-21 11:25:25 +0100

    better buttons layout

--- src/components/ConversationTree.tsx
+++ src/components/ConversationTree.tsx
@@ -6,7 +6,6 @@ import { useConversationTree } from '../hooks/useConversationTree';
 import { createContextMenuHandler, checkNodes } from '../utils/conversationTreeHandlers';
 import { createNodesInOrder } from '../utils/nodeCreation';
 import { calculateSteps } from '../utils/nodeNavigation';
-import { RefreshButton } from './RefreshButton';
 import { ExportButton } from './ExportButton';
 import { CustomNode } from "./CustomNode";
 import { SearchBar } from './SearchBar';
@@ -133,8 +132,31 @@ const ConversationTree = () => {
 
   return (
     <div className="w-full h-full" style={{ height: '100%', width: '100%' }}>
-      <RefreshButton onClick={handleRefresh} />
-      <ExportButton nodes={nodes} conversationData={conversationData} />
+      <div className="absolute top-4 right-4 flex items-center bg-white rounded-lg shadow-lg divide-x divide-gray-200 z-10">
+        <button
+          onClick={() => setShowSearch(true)}
+          className="p-2.5 hover:bg-gray-50 transition-colors rounded-l-lg group"
+          title="Search messages (⌘+K)"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-600 group-hover:text-gray-800" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+          </svg>
+        </button>
+        <button
+          onClick={handleRefresh}
+          className="p-2.5 hover:bg-gray-50 transition-colors group"
+          title="Refresh conversation"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-600 group-hover:text-gray-800" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
+          </svg>
+        </button>
+        <ExportButton 
+          nodes={nodes} 
+          conversationData={conversationData}
+          className="p-2.5 hover:bg-gray-50 transition-colors rounded-r-lg group"
+        />
+      </div>
       <ReactFlow
         ref={ref}
         nodes={nodes}
